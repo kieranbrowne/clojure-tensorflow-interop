334441411795aa3ef2c0a05fa8fb98c4bf9e1739
Kieran Browne <kierantbrowne@gmail.com>
2017-03-29 22:35:09 +1100

    Experimenting with macros for clj -> tensorflow

--- src/clojure_tensorflow_interop/core.clj
+++ src/clojure_tensorflow_interop/core.clj
@@ -54,6 +54,14 @@
     (Tensor/create
     (int-array [8 2]))))
 
+(Tensor/create (to-nested-ints [1 2]))
+
+(map type (to-nested-ints [1 2]))
+(type (to-array (to-nested-ints [1 2])))
+
+
+
+
 (def tensor-2
   (outputify "t2"
    (Tensor/create
@@ -112,3 +120,159 @@
 ;; hood.
 
 ;; TODO
+
+;; As a simple example of deep learning, lets train a network on the XOR
+;; gate.
+
+
+(def graph (new Graph))
+
+(defn outputify [name tensor]
+  (->
+   (.opBuilder graph "Const" name)
+   (.setAttr "dtype" (.dataType tensor))
+   (.setAttr "value" tensor)
+   .build
+   (.output 0)
+   ))
+
+(def training-input
+  (outputify "train-in"
+             (Tensor/create
+              (to-array (map int-array [[0 0]
+                                        [1 0]
+                                        [0 1]
+                                        [1 1]])))))
+
+(def training-output
+  (outputify "train-out"
+             (Tensor/create
+              (to-array (map int-array [[0]
+                                        [1]
+                                        [1]
+                                        [0]])))))
+
+(def synapses-0
+  (outputify "syn0"
+             (Tensor/create
+              (to-array (map int-array [[2 2]]))
+              )))
+
+
+(def divide
+  (->
+   (.opBuilder graph "Div" "my-dividing-operation")
+   (.addInput tensor-1)
+   (.addInput tensor-2)
+   .build
+   (.output 0)
+   ))
+
+;; activation op
+(def graph (new Graph))
+(def divide
+  (->
+   (.opBuilder graph "Tanh" "dicks")
+   (.addInput tensor-1)
+   .build
+   (.output 0)
+   ))
+
+(def session (new Session graph))
+
+
+(def result
+  (-> session
+      .runner
+      (.fetch (.name (.op divide)))
+      .run
+      (.get 0)
+      (.copyTo
+       (make-array java.lang.Integer/TYPE 1 2))
+      ))
+
+(pprint result)
+(count [[1 1] [1]])
+
+
+(counted? (to-array [1 2]))
+(defn counted?)
+(if (try (inc (count [1])) (catch Exception e false)) true false)
+
+(contains? #{1 2 7} 6)
+;; We need a fn to turn clojure data structures to types
+;; that TensorFlow accepts
+(coll? 1)
+(defn tf-vals [v]
+  (cond
+    ;; list of some kind
+    ;; (contains? #{"clojure.lang.PersistentVector"
+    ;;              "clojure.lang.PersistentList"}
+    ;;            (.getName (type v)))
+    (coll? v)
+    (if (coll? (first v))
+      (to-array (map tf-vals v))
+      (case (.getName (type (first v)))
+        "java.lang.Long" (int-array v)
+        "java.lang.Double" (float-array v)))
+    (= (.getName (type v)) "java.lang.Long") (int v)
+    (= (.getName (type v)) "java.lang.Double") (int v)
+    ;; anything else
+    true v))
+
+;; potential clj-to-ops macro
+(defn fn-to-op [f]
+  (case f
+    * "Mult"
+    / "Div"
+    + "Add"
+    def "Const"
+    java.lang.Math/tanh "Tanh"
+    f))
+
+(. (class 1) TYPE)
+(type (tf-vals 1))
+(type (int-array (tf-vals [1 2])))
+(type (tf-vals [[1 2]]))
+
+
+(defn prep-for-tf
+  "Take clojure val and return tensorflow constant (Operation)"
+  [val]
+  (outputify (str (gensym))
+   (Tensor/create (tf-vals val))))
+
+(outputify "ballz"
+ (Tensor/create (tf-vals [[1]])))
+
+(prep-for-tf 1)
+(prep-for-tf [1])
+(prep-for-tf [[1]])
+
+
+
+(defmacro clj-to-ops
+  "UNFINISHED: I'm curious whether we can turn standard
+  Clojure code into TensorFlow operations."
+  [sexp graph]
+  (let [op (fn-to-op (first sexp))
+        inputs (rest sexp)]
+    (list 'let (vector 'args (vec inputs))
+    (concat (list '-> (list '.opBuilder graph op (str (gensym))))
+            (map #(list '.addInput
+                        (list 'prep-for-tf
+                              (list 'get 'args %1)))
+                 (range (count inputs)))
+          (list '.build '(.output 0))))))
+
+(macroexpand-1 '(clj-to-ops (* 1 (* 1 3)) graph))
+(macroexpand '(clj-to-ops (* 1 (* 1 3)) graph))
+(macroexpand-1 '(clj-to-ops (+ 1 1) graph))
+(macroexpand '(clj-to-ops (* 1 (* 1 3)) graph))
+
+
+(clj-to-ops (+ 1 2) graph)
+
+(map-indexed #(str %1 %2) (range 1 8))
+
+(vector 1 2)
