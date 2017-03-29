0fd51e4e3e5e2ba6466286b5cef07492a411ff60
Kieran Browne <kierantbrowne@gmail.com>
2017-03-30 00:42:50 +1100

    More musing; a bit of shuffling.

--- src/clojure_tensorflow_interop/core.clj
+++ src/clojure_tensorflow_interop/core.clj
@@ -22,9 +22,10 @@
 ;; The main object of computation in TensorFlow is the *tensor*.
 ;; A Tensor is just a typed multi-dimensional array. Nothing scary here.
 
-;; When we write in TensorFlow, we're not actually running computations.
-;; Instead we'll represent our computations as a *graph*.
-;; This graph will describe the flow of our data through a series of
+;; When we write code for TensorFlow, we're not actually running
+;; computations. Instead we're composing a data structure which
+;; describes the flow of data. In TensorFlow this is called a *graph*.
+;; The graph will describe the flow of our data through a series of
 ;; operations (*ops* for short). Nothing will actually be computed
 ;; until we launch our graph in a *session*. The session handles the
 ;; execution of our graph on the CPU or GPU and returns the resulting
@@ -34,52 +35,63 @@
 ;; to the C code using a Session object.
 ;; Let's get started.
 
+;; For demonstration purposes I'm going to do the first example without
+;; abstracting my code. It ain't pretty, but it should make the process
+;; clearer.
+
 
 ;; First we need to initialise a new Graph object
 (def graph (new Graph))
 
-(defn outputify [name tensor]
-  (->
-   (.opBuilder graph "Const" name)
-   (.setAttr "dtype" (.dataType tensor))
-   (.setAttr "value" tensor)
-   .build
-   (.output 0)
-   ))
+;; Next we're going to need some example tensors to work with.
+;; Because the computation isn't running in clojure we can't just define
+;; our values. Instead we're defining an operation node in the graph
+;; that generates a constant.
+;; First I'm creating a tensor object using class' the create method.
+;; Because we're interopping with the java class we first need to turn
+;; our clojure persistant vector into an array of 32bit Integers.
+;; Using the arrow macro for clarity; we call the .opBuilder method on
+;; our graph, the first argument is the binary operation which will run
+;; in this case, its "Const". This is one of a set of possible binary ops
+;; that have been implemented in native code.
+;; The second argument is a name for the operation. I went with
+;; "tensor-1" for simplicity, but "Joaquin Phoenix" would
+;; have also worked. The only requirement is that it is unique to
+;; the graph. Next we set the value and datatype attributes
+;; that are required for the Const operation. Finally we build our
+;; operation based on the attributes and use the output method to return
+;; it. It is this returned operation that gets saved in clojure.
 
-;; Lets get a really basic operation running.
-;; We're also going to need some example tensors to work with
 (def tensor-1
-  (outputify "t1"
-    (Tensor/create
-    (int-array [8 2]))))
-
-(Tensor/create (to-nested-ints [1 2]))
-
-(map type (to-nested-ints [1 2]))
-(type (to-array (to-nested-ints [1 2])))
-
-
-
+  (let [tensor
+        (Tensor/create
+         (int-array
+          [360 909 216 108 777 132 256 174 999 228 324 800 264]
+          ))]
+    (-> graph
+     (.opBuilder "Const" "tensor-1")
+     (.setAttr "dtype" (.dataType tensor))
+     (.setAttr "value" tensor)
+     .build
+     (.output 0))))
 
 (def tensor-2
-  (outputify "t2"
-   (Tensor/create
-    (int-array [2 2]))))
+  (let [tensor
+        (Tensor/create
+         (int-array [5 9 2 1 7 3 8 2 9 2 3 8 8]))]
+    (-> graph
+        (.opBuilder "Const" "tensor-2")
+        (.setAttr "dtype" (.dataType tensor))
+        (.setAttr "value" tensor)
+        .build
+        (.output 0))))
 
-
-;; Now lets add an operation to our graph.
-;; The .opBuilder method is key here. It adds a new operation
-;; to our graph object, taking an Operation and Name as args.
-;; The operation name is one of a set of possible binary ops
-;; that have been implemented in native code. I'm going to use
-;; "Div" (the division operation) for my example. The second
-;; argument is a name for the operation; this is a string that
-;; the graph uses to keep track of it's parts.
-;; Following our opBuilder method, we use the .addInput method
-;; to add our two example tensors as input to the operation.
-;; Finally we run the build and output methods to complete the
-;; operation.
+;; Now lets add a more exciting operation to our graph.
+;; Again we will call the .opBuilder method on our graph object.
+;; I'm going to use the "Div" (division) operation this time.
+;; Next we call the .addInput method to add our two example tensors
+;; as input to the operation.
+;; Again we build and output our operation object, saving it as "divide".
 (def divide
   (->
    (.opBuilder graph "Div" "my-dividing-operation")
@@ -89,37 +101,128 @@
    (.output 0)
    ))
 
-;; Lastly, lets initialise a session object based on our our
-;; graph object.
+;; To run our newly built operations, we need to create a session object
+;; based on our graph.
 (def session (new Session graph))
 
 
-;; Get the results
+;; We'll call the .runner method on our session to get the engine running.
+;; We use the .fetch method to retrieve the divide operation by name;
+;; in this case we want to pass it the name we gave to the divide
+;; operation just before ("my-dividing-operation"). The .get method
+;; gets our result from the returned array, this gives us a Tensor object
+;; which has all the data but cannot be read easily, so finally to
+;; read our results, we call the .copyTo method on the Tensor to
+;; copy the contents to an integer array.
 (def result
   (-> session
       .runner
-      (.fetch (.name (.op divide)))
+      (.fetch "my-dividing-operation")
       .run
       (.get 0)
-      (.copyTo (int-array 2))
+      (.copyTo (int-array 13))
       ))
 
-;;Read results
-(pprint result)
-;; => prints: [4, 1]
+;; Finally we can read our results.
+(apply str (map char result))
+;; => "Hello, World!"
+
+;; So we successfully run a basic TensorFlow graph, but that code made my
+;; eyes bleed. This is partially because the TensorFlow Java api is so
+;; new and doesn't have the multitudes of helper functions that python
+;; has yet.
+;; But for me, is that I write clojure to get away from object
+;; oriented programming.
+
+;; We have all that we need from the Java api already. We can already
+;; work with all binary operations, run sessions and even load existing
+;; and/or pre-trained models from TensorFlow in any other language.
+
+;; There's a real opportunity here to make a properly clojurian api.
+;; There are a couple of things that I think make TensorFlow and Clojure
+;; a great match.
+;; TensorFlow's graph concept maps well to data structure
+;; structure, and no programming language has a better story for working
+;; with data structures than clojure.
+
+;; The code is data 
+
+;; And then, with a short macro we can turn our
+;; (clj->ops (/ [8 2] [2 2]))
+;; => [4 1]
+
+;; One exciting direction for this is the
+;; Lisp's whole *code is data* thing.
+;; Below, I've hashed out a proof of concept macro to turn a clojure
+;; s-expression into a big hairy TensorFlow operation.
+
+
+;; We need a fn to turn clojure data structures to types
+;; that TensorFlow accepts
+(defn tf-vals [v]
+  (cond
+    (coll? v)
+    (if (coll? (first v))
+      (to-array (map tf-vals v))
+      (case (.getName (type (first v)))
+        "java.lang.Long" (int-array v)
+        "java.lang.Double" (float-array v)))
+    (= (.getName (type v)) "java.lang.Long") (int v)
+    (= (.getName (type v)) "java.lang.Double") (int v)
+    ;; anything else
+    true v))
+
+;; potential clj-to-ops macro
+
+
+(defn prep-for-tf
+  "Take clojure val and return tensorflow constant (Operation)"
+  [val]
+  (outputify (str (gensym))
+             (Tensor/create (tf-vals val))))
+
+
+(prep-for-tf 1)
+(prep-for-tf [1])
+(prep-for-tf [[1]])
+
+;; To do this I first need to tell clojure about some stuff.
+(defn fn-to-op [f]
+  (case f
+    * "Mult"
+    / "Div"
+    + "Add"
+    def "Const"
+    java.lang.Math/tanh "Tanh"
+    f))
+
+(defmacro clj->ops
+  "UNFINISHED: I'm curious whether we can turn standard
+  Clojure code into TensorFlow operations."
+  [sexp graph]
+  (let [op (fn-to-op (first sexp))
+        inputs (rest sexp)]
+    (list 'let (vector 'args (vec inputs))
+    (concat (list '-> (list '.opBuilder graph op (str (gensym))))
+            (map #(list '.addInput
+                        (list 'prep-for-tf
+                              (list 'get 'args %1)))
+                 (range (count inputs)))
+          (list '.build '(.output 0))))))
+
+
+(macroexpand '(clj->ops (+ 1 1) graph))
+;; => (let [args [1 1]] (-> (.opBuilder graph "Add" "G__25782") (.addInput (prep-for-tf (get args 0))) (.addInput (prep-for-tf (get args 1))) .build (.output 0)))
+
+(clj->ops (+ 1 2) graph)
 
 
 
-;; It's still very early days for
-;; the Java api so we don't have many of the helper functions that Python
-;; users get. To make up for that, lets define a few helper functions.
 
-;; We can create tensors using the create method from the Tensor class.
-;; Because a tensor is a "typed multi-dimensional array" we can't just
-;; use clojure data structures, which are actually not arrays under the
-;; hood.
 
-;; TODO
+
+
+
 
 ;; As a simple example of deep learning, lets train a network on the XOR
 ;; gate.
@@ -190,89 +293,3 @@
       (.copyTo
        (make-array java.lang.Integer/TYPE 1 2))
       ))
-
-(pprint result)
-(count [[1 1] [1]])
-
-
-(counted? (to-array [1 2]))
-(defn counted?)
-(if (try (inc (count [1])) (catch Exception e false)) true false)
-
-(contains? #{1 2 7} 6)
-;; We need a fn to turn clojure data structures to types
-;; that TensorFlow accepts
-(coll? 1)
-(defn tf-vals [v]
-  (cond
-    ;; list of some kind
-    ;; (contains? #{"clojure.lang.PersistentVector"
-    ;;              "clojure.lang.PersistentList"}
-    ;;            (.getName (type v)))
-    (coll? v)
-    (if (coll? (first v))
-      (to-array (map tf-vals v))
-      (case (.getName (type (first v)))
-        "java.lang.Long" (int-array v)
-        "java.lang.Double" (float-array v)))
-    (= (.getName (type v)) "java.lang.Long") (int v)
-    (= (.getName (type v)) "java.lang.Double") (int v)
-    ;; anything else
-    true v))
-
-;; potential clj-to-ops macro
-(defn fn-to-op [f]
-  (case f
-    * "Mult"
-    / "Div"
-    + "Add"
-    def "Const"
-    java.lang.Math/tanh "Tanh"
-    f))
-
-(. (class 1) TYPE)
-(type (tf-vals 1))
-(type (int-array (tf-vals [1 2])))
-(type (tf-vals [[1 2]]))
-
-
-(defn prep-for-tf
-  "Take clojure val and return tensorflow constant (Operation)"
-  [val]
-  (outputify (str (gensym))
-   (Tensor/create (tf-vals val))))
-
-(outputify "ballz"
- (Tensor/create (tf-vals [[1]])))
-
-(prep-for-tf 1)
-(prep-for-tf [1])
-(prep-for-tf [[1]])
-
-
-
-(defmacro clj-to-ops
-  "UNFINISHED: I'm curious whether we can turn standard
-  Clojure code into TensorFlow operations."
-  [sexp graph]
-  (let [op (fn-to-op (first sexp))
-        inputs (rest sexp)]
-    (list 'let (vector 'args (vec inputs))
-    (concat (list '-> (list '.opBuilder graph op (str (gensym))))
-            (map #(list '.addInput
-                        (list 'prep-for-tf
-                              (list 'get 'args %1)))
-                 (range (count inputs)))
-          (list '.build '(.output 0))))))
-
-(macroexpand-1 '(clj-to-ops (* 1 (* 1 3)) graph))
-(macroexpand '(clj-to-ops (* 1 (* 1 3)) graph))
-(macroexpand-1 '(clj-to-ops (+ 1 1) graph))
-(macroexpand '(clj-to-ops (* 1 (* 1 3)) graph))
-
-
-(clj-to-ops (+ 1 2) graph)
-
-(map-indexed #(str %1 %2) (range 1 8))
-
-(vector 1 2)
